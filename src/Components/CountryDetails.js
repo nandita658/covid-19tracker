1edc96e31b4b4444e065141ed53680a59b5572c5
nandita658 <jainnandita225@gmail.com>
2022-01-07 17:17:45 +0530

    href

--- src/Components/CountryDetails.js
+++ src/Components/CountryDetails.js
@@ -12,15 +12,15 @@ const countryDetails = (props) => {
             </div>
             <div className={classes.casesDetails}>
                 <div className={classes.casesBox}>
-                    <a href="#">{props.totalCase}</a>
+                    <a href="#/">{props.totalCase}</a>
                     <p className="yesterday">Last 24 hours: <strong>{props.newCase}</strong></p>
                 </div>
                 <div className={classes.casesBox}>
-                <a href="#">{props.totalDeaths}</a>
+                <a href="#/">{props.totalDeaths}</a>
                     <p className="yesterday">Last 24 hours: <strong>{props.newDeaths}</strong></p>
                 </div>
                 <div className={classes.casesBox}>
-                <a href="#">{props.totalRecovered}</a>
+                <a href="#/">{props.totalRecovered}</a>
                     <p className="yesterday">Last 24 hours: <strong>{props.newRecovered}</strong></p>
                 </div>
             </div>
